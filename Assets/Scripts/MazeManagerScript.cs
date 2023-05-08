896a539091fcab3b06f3b68a73fd899d4c0f2644
Frederik Hess <cph-fp103@cphbusiness.dk>
2023-05-08 13:39:24 +0200

    Tried fixing walls on maze2

--- Assets/Scripts/MazeManagerScript.cs
+++ Assets/Scripts/MazeManagerScript.cs
@@ -1,6 +1,7 @@
 using Photon.Pun;
 using Photon.Realtime;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 public class MazeManagerScript : MonoBehaviour
 {
     public GameObject maze1;
@@ -8,11 +9,19 @@ public class MazeManagerScript : MonoBehaviour
 
     void Start()
     {
+        if (!PhotonNetwork.IsConnected)
+        {
+            SceneManager.LoadScene("Loading");
+ 
+            return;
+        }
+
         PhotonNetwork.ConnectUsingSettings();
     }
     private void Awake() {
          int randomIndex = Random.Range(0, 2);
         Debug.Log(randomIndex);
+        randomIndex = 0;
         if (randomIndex == 0)
         {
             Instantiate(maze1, Vector3.zero, Quaternion.identity);
